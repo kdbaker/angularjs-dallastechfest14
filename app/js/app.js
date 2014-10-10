064ee80db1ac28cc281a9eed128f5dcac641f381
Kevin Baker <kevin.baker@improvingenterprises.com>
2014-10-10 14:18:32 -0500

    level0

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* App Module */
+var dtf = angular.module( 'dtf', [ ] );
+
+/* App Controller */
+dtf.controller( 'AppCtrl',
+  [ '$scope',
+  function ( $scope ) {
+
+  }]);
---
+++ test/e2e/scenarios.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* https://docs.angularjs.org/guide/e2e-testing */
+
+describe( 'DTF App', function() {
+
+  beforeEach( function() {
+    browser.get( 'app/index.html' );
+  });
+
+});
